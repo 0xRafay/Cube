57671bed4f9d3fdfa69efc6847a7014889a2db1e
0xRafay <abdulrafay.src@gmail.com>
2026-07-18 10:33:17 -0400

    feat: add perspective projection formulas
    - for both 2D & 3D points

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <math.h>
 #include <stdio.h>
 
-/* Perspective projection is usually categorized into one-point, two-point and three-point perspective,
- * depending on the orientation of the projection plane towards the axes of the depicted object.
+/* Perspective projection is usually categorized into one-point, two-point and
+ * three-point perspective, depending on the orientation of the projection plane
+ * towards the axes of the depicted object.
  */
 
 /*
@@ -14,13 +15,41 @@ typedef struct {
   float z;
 } points3D;
 
+typedef struct {
+  float x;
+  float y;
+} points2D;
+
 // 3D points to be projected onto 2D plane
 float A, B, C;
 float cubeWidth = 20;
 
 int main() {
-  points3D vertices[8] = {{}, {}, {}, {}, {}, {}, {}, {}};
+  points3D vertices3D[8] = {
+      {-10, 10, 20}, {10, 10, 20}, {10, -10, 20}, {-10, -10, 20},
+
+      {-10, 10, 40}, {10, 10, 40}, {10, -10, 40}, {-10, -10, 40}};
+
+  points2D vertices2D[] = {};
+
+  int centerX = 40;
+  int centerY = 12;
+
+  float screenX;
+  float screenY;
+
+  for (int i = 0; i < 8; i++) {
+    screenX = (((vertices3D[i].x) * 200) / vertices3D[i].z) + centerX;
+    vertices2D[i].x = screenX;
+
+    screenY = (((centerY - (vertices3D[i].y)) * 200) / vertices3D[i].z);
+    vertices2D[i].y = screenY;
+  }
+
+  for (;;) {
   printf("\x1b[2J");
+  }
+  printf("");
 
   return 0;
 }
