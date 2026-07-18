5e6d15531f9f00d9ed2624a7c0d3e01e4c7578fe
0xRafay <abdulrafay.src@gmail.com>
2026-07-18 09:56:20 -0400

    feat: added some context

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+
+/* Perspective projection is usually categorized into one-point, two-point and three-point perspective,
+ * depending on the orientation of the projection plane towards the axes of the depicted object.
+ */
+
+/*
+ * the 3D position of a point A that is to be projected
+ */
 typedef struct {
   float x;
   float y;
   float z;
 } points3D;
 
-int main() { return 0; }
+// 3D points to be projected onto 2D plane
+float A, B, C;
+float cubeWidth = 20;
+
+int main() {
+  points3D vertices[8] = {{}, {}, {}, {}, {}, {}, {}, {}};
+  printf("\x1b[2J");
+
+  return 0;
+}
